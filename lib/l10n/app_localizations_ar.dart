e0e24b57fb1afbdeb698138dc07133e3dc6af0b5
LanaBallouli <159704744+LanaBallouli@users.noreply.github.com>
2025-04-15 00:02:25 +0300

    add

--- lib/l10n/app_localizations_ar.dart
+++ lib/l10n/app_localizations_ar.dart
@@ -8,4 +8,5 @@ import 'app_localizations.dart';
 class AppLocalizationsAr extends AppLocalizations {
   AppLocalizationsAr([String locale = 'ar']) : super(locale);
 
+
 }
--- lib/l10n/app_localizations_en.dart
+++ lib/l10n/app_localizations_en.dart
@@ -8,4 +8,5 @@ import 'app_localizations.dart';
 class AppLocalizationsEn extends AppLocalizations {
   AppLocalizationsEn([String locale = 'en']) : super(locale);
 
+
 }
